a829b623d073f30809272df260bbb73ab7158a85
SamarSingla0 <samarsingla04@gmail.com>
2025-12-01 18:15:12 +0530

    deployment 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,6 +133,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOWED_ORIGINS = [
+   "https://your-frontend.vercel.app",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
